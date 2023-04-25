596fe6cd5b25b578305f86192cd16215895a16f3
Muhammadaminjon <muhammadaminovmuhadamin303@gamil.com>
2023-04-25 15:29:30 +0500

    new choose and comments section with styles

--- js/main.js
+++ js/main.js
@@ -1,30 +1,39 @@
-// home swiper js
+// main products section swiper js
 const swiper = new Swiper(".swiper", {
   slidesPerView: "auto",
   speed: 1000,
   spaceBetween: 20,
-  // Navigation arrows
   navigation: {
     nextEl: ".button-next",
     prevEl: ".button-prev",
   },
 });
+// main comments section swiper js
+const mySwiper = new Swiper(".mySwiper", {
+  slidesPerView: 'auto',
+  speed: 800,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".comment-next",
+    prevEl: ".comment-prev",
+  },
+});
 
-const scroll = document.querySelector('.bg-circle')
-const nav = document.getElementById('navbar')
+const scroll = document.querySelector(".bg-circle");
+const nav = document.getElementById("navbar");
 
-window.addEventListener('scroll',(e)=>{
-  if(window.scrollY >= 300) {
-    scroll.classList.add("bghh")
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY >= 300) {
+    scroll.classList.add("bghh");
   } else {
-    scroll.classList.remove("bghh")
+    scroll.classList.remove("bghh");
   }
 });
 
-window.addEventListener('scroll',(e)=>{
-  if(window.scrollY >= 100) {
-    nav.classList.add("navbar")
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY >= 100) {
+    nav.classList.add("navbar");
   } else {
-    nav.classList.remove("navbar")
+    nav.classList.remove("navbar");
   }
-})
+});
